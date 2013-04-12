56f9eab5607a85c40eb0ba60cc2844c4af95bbea
dozedoff <dozedoffagain@gmail.com>
2013-04-12 11:23:50 +0200

    Added magnet link to target types, added  comments

--- src/main/java/com/github/dozedoff/results/TargetType.java
+++ src/main/java/com/github/dozedoff/results/TargetType.java
@@ -18,5 +18,8 @@
 package com.github.dozedoff.results;
 
 public enum TargetType {
-	TORRENT, FILE, DOWNLOAD_LINK
+	TORRENT, // a link to a torrent file
+	MAGNET_LINK, // a magnet link for torrents
+	FILE,	// a direct link to the file
+	DOWNLOAD_LINK	// a link to a download site
 }
