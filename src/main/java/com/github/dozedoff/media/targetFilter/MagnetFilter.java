a31bd4877285ef9b630853d5d19b15617db1d373
dozedoff <dozedoffagain@gmail.com>
2013-04-13 20:26:01 +0200

    Added license text

--- src/main/java/com/github/dozedoff/media/targetFilter/MagnetFilter.java
+++ src/main/java/com/github/dozedoff/media/targetFilter/MagnetFilter.java
@@ -1,5 +1,32 @@
+/*  Copyright (C) 2013  Nicholas Wright
+    
+    This file is part of mmut - Multi-media update tracker
+    
+    mmut is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 package com.github.dozedoff.media.targetFilter;
 
-public class MagnetFilter {
+import java.util.LinkedList;
+import java.util.List;
 
+import org.jsoup.nodes.Element;
+
+public class MagnetFilter implements TargetFilter{
+	@Override
+	public List<Element> parse() {
+		LinkedList<Element> results = new LinkedList<>();
+		
+		return results;
+	}
 }
